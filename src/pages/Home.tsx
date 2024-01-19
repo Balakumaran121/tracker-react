481da8abd63a75430bd0e558bbb48b51a34212bc
Balakumaran <bkumaran121@gmail.com>
2024-01-19 20:49:02 +0530

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <nav>
+        <h1>Todo Tracker</h1>
+        <ul>
+          <li>Login</li>
+          <li>Register</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
