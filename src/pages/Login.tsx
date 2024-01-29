a7d8863e1191403dc75a6e03c651da653f1ce026
Balakumaran <bkumaran121@gmail.com>
2024-01-29 20:56:45 +0530

    updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,15 @@
 const Login = () => {
   return (
-    <div className="flex justify-center">
-      <div className="w-auto min-h-96 bg-slate-200 mt-24 p-10">
-        <div className=" text-2xl font-bold flex justify-center -mb-4">
-          Login Form
+    <div className="flex justify-center bg-[#F0EEEB]">
+      <div className="w-[512px] h-[622px]  bg-[#FFFFFF] m-20  rounded-3xl flex flex-col items-center gap-16">
+        <div className=" text-[35px] font-bold flex justify-center pt-[97px]">
+          Login
         </div>
-        <div className="pt-20 pb-8 flex flex-col">
-          <input className="w-60 h-10 -mt-10 mb-2 rounded-md border-2 border-black" type="email" placeholder="Email address" />
-          <input className="w-60 h-10 mb-2 rounded-md border-2 border-black" type="password" placeholder="Password" />
-          
-        </div>
-        <div className="flex justify-center">
-          <button type="submit" className="w-24 h-8 bg-purple-700 hover:bg-black text-white rounded-md border-2 border-black">Login</button>
-        </div>
-        <div><p className=" font-medium text-slate-600 mt-4">Dont have account?<a href="#"className="font-medium text-purple-600 hover:underline"> SignIn </a>here</p></div>
+        <input className="border-b-2 w-[400px] font-semibold text-2xl" placeholder="Email"/>
+        <input className="border-b-2 w-[400px] font-semibold text-2xl" placeholder="Password"/>
+        <span className="font-bold text-xl">Forgot Password?</span>
+
+        <div className=" rounded-md  bg-[#F65D95] p-[40px] w-45">Login</div>
       </div>
       </div>
   )
