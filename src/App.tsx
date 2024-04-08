527e14f60f3e4e8d31bee7e669d22d40a3f43801
sulaiman <128182024+amds@users.noreply.github.com>
2024-04-09 05:03:01 +0530

    Newly Add Homepage Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import MainRoutes from "./routes"
+import Header from "./Components/Header"
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import Category from "./Components/Category"
+import FlashSales from "./Components/FlashSales"
+import Browse from "./Components/Browse"
+import BestSales from "./Components/BestSales"
+import FeatureSection from "./Components/FeatureSection"
+import Banner from "./Components/Banner"
+import Banner2 from "./Components/Banner2"
+import LastSection from "./Components/LastSection"
+import Footer from "./Components/Footer"
+
 
 const App = () => {
-  return <MainRoutes />
+  return (
+    <main>
+      <Header/>
+      <Navbar/>
+      <Hero/>
+      <FlashSales/>
+      <Browse/>
+      <BestSales/>
+      <Banner/>
+      <Category/>
+      <FeatureSection/>
+      <Banner2/>
+      <LastSection/>
+      <Footer/>
+      
+    
+      
+     
+    </main>
+  )
 }
 
 export default App
